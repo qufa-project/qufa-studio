9295c748fce84cd3d6f3c648580024ee178c0c1f
iskim1018 <iskim1018@gmail.com>
2021-08-23 11:31:28 +0900

    File 선택에 따른 Meta 설정 화면 구성

---
+++ public/javascripts/app/handlebars-helper.js
@@ -0,0 +1,18 @@
+Handlebars.registerHelper("select", function (value, options) {
+  var $el = $("<select />").html(options.fn(this));
+  $el.find('[value="' + value + '"]').attr({ selected: "selected" });
+  return $el.html();
+});
+
+Handlebars.registerHelper("math", function (lvalue, operator, rvalue, options) {
+  lvalue = parseFloat(lvalue);
+  rvalue = parseFloat(rvalue);
+
+  return {
+    "+": lvalue + rvalue,
+    "-": lvalue - rvalue,
+    "*": lvalue * rvalue,
+    "/": lvalue / rvalue,
+    "%": lvalue % rvalue,
+  }[operator];
+});
