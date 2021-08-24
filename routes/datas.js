1a4c538e6340d9355765a6d40d8c3196bbb0fb71
iskim1018 <iskim1018@gmail.com>
2021-08-24 11:41:25 +0900

    메타 전송 처리 추가

--- routes/datas.js
+++ routes/datas.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var router = express.Router();
-var multer = require('multer');
+var multer = require("multer");
 
-var storage = multer.memoryStorage()
-var upload = multer({ storage: storage })
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
 
-var FileManager = require("../lib/FileManager")
-var MetaManager = require("../lib/MetaManager")
+var FileManager = require("../lib/FileManager");
+var MetaManager = require("../lib/MetaManager");
 
 const child_process = require("child_process");
 
@@ -41,12 +41,12 @@ router.get("/", function (req, res, next) {
  *       200:
  *         description: Returns a mysterious string.
  */
-router.post("/", upload.single('file'), async function (req, res, next) {
+router.post("/", upload.single("file"), async function (req, res, next) {
   try {
     const response = await FileManager.uploadFile(req.file);
     console.log(response);
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
   res.json("upload success");
 });
@@ -61,23 +61,22 @@ router.post("/", upload.single('file'), async function (req, res, next) {
  *         description: Returns a mysterious string.
  */
 router.get("/meta", async function (req, res, next) {
-
-  res.json("success")
-  const key = '1628471368985_stat_new_0526.csv'
+  res.json("success");
+  const key = "1628471368985_stat_new_0526.csv";
   //const readStream = await FileManager.createReadStream(key);
   //detect file encoing using stream
-  ls = child_process.spawn('node',['./scripts/parseCsv.js', key])
-  ls.stdout.on('data', function (data) {
-    console.log('stdout: ' + data);
+  ls = child_process.spawn("node", ["./scripts/parseCsv.js", key]);
+  ls.stdout.on("data", function (data) {
+    console.log("stdout: " + data);
   });
 
-  ls.stderr.on('data', function (data) {
-    console.log('stderr: ' + data);
+  ls.stderr.on("data", function (data) {
+    console.log("stderr: " + data);
   });
 
-  ls.on('exit', function (code) {
-    console.log('exit: ' + code);
+  ls.on("exit", function (code) {
+    console.log("exit: " + code);
   });
-})
+});
 
 module.exports = router;
