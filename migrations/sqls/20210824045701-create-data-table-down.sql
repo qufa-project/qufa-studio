ffdffe684a79f1eab28b4fbc8207303748609368
iskim1018 <iskim1018@gmail.com>
2021-08-24 14:15:08 +0900

    data table 명 컬럼 추가

--- migrations/sqls/20210824045701-create-data-table-down.sql
+++ migrations/sqls/20210824045701-create-data-table-down.sql
@@ -1,1 +1,4 @@
+DROP INDEX data_name_idx;
+DROP INDEX data_data_table_idx;
+
 DROP TABLE `data`
--- migrations/sqls/20210824045701-create-data-table-up.sql
+++ migrations/sqls/20210824045701-create-data-table-up.sql
@@ -6,7 +6,14 @@ CREATE TABLE `data` (
   `file_size` BIGINT NOT NULL DEFAULT 0,
   `remote_path` VARCHAR(255),
   `origin_file_name` VARCHAR(255) NOT NULL,
+  `data_table` VARCHAR(255) NOT NULL,
   `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
   `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY(id)
 );
+
+CREATE UNIQUE INDEX data_name_idx
+ON `data`(`name`);
+
+CREATE UNIQUE INDEX data_data_table_idx
+ON `data`(`data_table`);
