3205a587f8146508943684b3921c43e833b33e24
iskim1018 <iskim1018@gmail.com>
2021-08-26 16:51:14 +0900

    Meta 상세 화면 추가

--- public/javascripts/app/handlebars-helper.js
+++ public/javascripts/app/handlebars-helper.js
@@ -16,3 +16,30 @@ Handlebars.registerHelper("math", function (lvalue, operator, rvalue, options) {
     "%": lvalue % rvalue,
   }[operator];
 });
+
+Handlebars.registerHelper("ifCond", function (v1, operator, v2, options) {
+  switch (operator) {
+    case "==":
+      return v1 == v2 ? options.fn(this) : options.inverse(this);
+    case "===":
+      return v1 === v2 ? options.fn(this) : options.inverse(this);
+    case "!=":
+      return v1 != v2 ? options.fn(this) : options.inverse(this);
+    case "!==":
+      return v1 !== v2 ? options.fn(this) : options.inverse(this);
+    case "<":
+      return v1 < v2 ? options.fn(this) : options.inverse(this);
+    case "<=":
+      return v1 <= v2 ? options.fn(this) : options.inverse(this);
+    case ">":
+      return v1 > v2 ? options.fn(this) : options.inverse(this);
+    case ">=":
+      return v1 >= v2 ? options.fn(this) : options.inverse(this);
+    case "&&":
+      return v1 && v2 ? options.fn(this) : options.inverse(this);
+    case "||":
+      return v1 || v2 ? options.fn(this) : options.inverse(this);
+    default:
+      return options.inverse(this);
+  }
+});
